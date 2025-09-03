6e8a170564d456194162f82f45530a6f4fc37ffe
msadeqsirjani <m.sadeq.sirjani@gmail.com>
2025-09-03 00:59:48 -0500

    [refactor]: update import statement in __init__.py to use MambaESR for consistency with recent model updates

--- src/__init__.py
+++ src/__init__.py
@@ -1,9 +1,9 @@
-from .model import MambaLiteSR
+from .model import MambaESR
 from .data import DF2KDataset, EvalImageFolder
 from .utils import calculate_psnr_y, calculate_ssim_y
 
 __all__ = [
-    "MambaLiteSR",
+    "MambaESR",
     "DF2KDataset",
     "EvalImageFolder",
     "calculate_psnr_y",
